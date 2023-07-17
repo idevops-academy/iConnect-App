47aeb4012064e9773eaba06af244dcd872dd0aa7
shaik447 <shaik.imp@gmail.com>
2023-07-17 20:24:16 +0000

    Update 4 files
    - /nginx/sysmon.conf
    - /install_dependencies.sh
    - /script_test.sh
    - /.gitlab-ci.yml

---
+++ install_dependencies.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Update the system
+sudo yum update -y
+
+# Install Git
+sudo yum install git -y
+
+# Install Node.js
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo -E bash -
+sudo yum install -y nodejs
+sudo npm install -g pm2
+
+
+# Install Nginx
+sudo yum install nginx -y
+
+# Start and enable Nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+# Display installed versions
+echo "Git version:"
+git --version
+echo "Node.js version:"
+node --version
+echo "Nginx version:"
+nginx -v
+
+#create directory for cloning the app
+sudo mkdir -p /var/myapp
+cd /var/myapp
+
+#clone the application from github
+# sudo git clone -b $branchname https://$username:$password@gitlab.com/shaik447/iConnect-App.git
+# cd iConnect-App/
+
