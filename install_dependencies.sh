07590b2617d9b8fe09f24a6a299b7025f5d152a0
shaik447 <shaik.imp@gmail.com>
2023-07-20 08:25:15 +0000

    Update file install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,23 +1,67 @@
 #!/bin/bash
 
+
+# Function to check if a package is installed
+is_package_installed() {
+    if [ -x "$(command -v $1)" ]; then
+        return 0 # Package is installed
+    else
+        return 1 # Package is not installed
+    fi
+}
+
 # Update the system
 sudo yum update -y
 
-# Install Git
-sudo yum install git -y
+# Install Git if not installed
+if ! is_package_installed git; then
+    echo "Installing Git..."
+    sudo yum install git -y
+else
+    echo "Git is already installed."
+fi
 
-# Install Node.js
-curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo -E bash -
-sudo yum install -y nodejs
-sudo npm install -g pm2
+# Install Node.js if not installed
+if ! is_package_installed node; then
+    echo "Installing Node.js..."
+    curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo -E bash -
+    sudo yum install -y nodejs
+else
+    echo "Node.js is already installed."
+fi
+
+# Install PM2 if not installed
+if ! is_package_installed pm2; then
+    echo "Installing PM2..."
+    sudo npm install -g pm2
+else
+    echo "PM2 is already installed."
+fi
+
+# Install Nginx if not installed
+if ! is_package_installed nginx; then
+    echo "Installing Nginx..."
+    sudo yum install nginx -y
+else
+    echo "Nginx is already installed."
+fi
 
 
-# Install Nginx
-sudo yum install nginx -y
+# # Install Git
+# sudo yum install git -y
 
-# Start and enable Nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
+# # Install Node.js
+# curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo -E bash -
+# sudo yum install -y nodejs
+# sudo npm install -g pm2
+
+
+# # Install Nginx
+# sudo yum install nginx -y
+
+# # Start and enable Nginx
+# sudo systemctl start nginx
+# sudo systemctl enable nginx
 
 # Display installed versions
 echo "Git version:"
