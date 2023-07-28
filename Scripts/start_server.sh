ec7ada9c24b1df9268abb7d6fa7c6da62b2a0020
shaik447 <shaik.imp@gmail.com>
2023-07-28 14:46:17 +0000

    Update file start_server.sh

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -5,7 +5,7 @@ echo "Copying nginx config to /etc/nginx/conf.d directory"
 sudo cp nginx/sysmon.conf /etc/nginx/conf.d/
 
 echo "start nodejs application as a process using PM2"
-sudo pm2 start --name iconnect-<commit-sha> npm -- start
+sudo pm2 start --name iconnect npm -- start
 
 echo "restarting nginx..."
 sudo systemctl restart nginx
